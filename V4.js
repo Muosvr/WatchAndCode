e0b926afe848f8c5af5867a2f9e610032933cb06
Jason Wu <lujason2015@gmail.com>
2018-05-04 01:23:56 +0000

    second commit

--- V4.js
+++ V4.js
@@ -1,3 +1,9 @@
+//requirements:
+// todoList.addToDo should add objects
+// todoList.changeTodo should change the todo Text property
+// todoList.toggleCompleted should change the completed property
+
+
 var todoList = {
   todos: [],
   displayTodos: function() {
