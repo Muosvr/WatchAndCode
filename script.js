27615ca82c70e4e995b90e05327f6b9e030bbbd3
Jason Wu <lujason2015@gmail.com>
2018-05-14 23:11:45 +0000

    V10

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 
 var todoList = {
   todos: [],
-  // displayTodos: function() {
-    
-  //   if (this.todos.length === 0){
-  //     console.log("Todo list is empty");
-  //   }else {
-  //     for (i = 0; i<this.todos.length; i++){
-  //       if(this.todos[i].completed === true){
-  //         console.log("(X)",this.todos[i].todoText);
-  //       }else {
-  //         console.log("( )",this.todos[i].todoText);
-  //       }
-        
-        
-  //     }
-  //   }
-  // },
   
   addTodo: function(todoText) {
     this.todos.push({
